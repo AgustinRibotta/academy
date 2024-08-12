9dc72a648799059a3e07eb97b8b8121ab1059865
Agustin Ribotta <agustin.ribotta.r@gmail.com>
2024-08-12 23:45:35 +0200

    CRUD user

--- apps/teacher/models.py
+++ apps/teacher/models.py
@@ -1,18 +1,17 @@
 from django.db import models
-from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.hashers import make_password, check_password
 
 class Teacher(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=255)
 
-    fname = models.CharField(_("First Name"), max_length=50)
-    lname = models.CharField(_("Last Name"), max_length=50)
-    username =  models.CharField(_("User Name"), max_length=50)
-    password =  models.TextField(_("Passowrd"))
-    
+    def set_password(self, raw_password):
+        self.password = make_password(raw_password)
 
-    class Meta:
-        verbose_name = _("Teacher")
-        verbose_name_plural = _("Teachers")
+    def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
 
     def __str__(self):
-        return self.name
-
+        return f"{self.fname} {self.lname}"
